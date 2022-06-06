d7790b2f3af063e867da33ec242f25b9b4fd8a26
Combatd <combatdstar@gmail.com>
2022-06-05 17:42:35 -0700

    Define #logBetween

---
+++ logBetween.js
@@ -0,0 +1,9 @@
+function logBetween(lowNum, highNum) {
+    for (let i = lowNum; i <= highNum; i++) {
+        console.log(i);
+    }
+}
+
+logBetween(-1, 2);
+logBetween(14, 6); 
+logBetween(4, 6); 
